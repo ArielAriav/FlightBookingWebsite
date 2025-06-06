6050d02005947d2685186234788732a6b896d76a
Babilabong <157375371+Babilabong@users.noreply.github.com>
2025-06-06 12:33:24 +0300

    fixing cicd 3

--- backend/server/test.js
+++ backend/server/test.js
@@ -3,7 +3,7 @@ const app = require("./app");
 
 describe("GET /api/flights", () => {
   it("should return an array (possibly empty)", async () => {
-    const res = await request(app).get("/api/flights");
+    const res = await request(app).get("/flights");
     expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
   });
