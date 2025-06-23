a11aca25fc9cb949bdbb9b6a463bcd81d44ae9c4
Babilabong <157375371+Babilabong@users.noreply.github.com>
2025-06-23 18:03:00 +0300

    bring from db only neccery flights

--- init.sql
+++ init.sql
@@ -20,9 +20,17 @@ CREATE TABLE IF NOT EXISTS bookings (
 -- הכנסת 1000 טיסות רנדומליות עם שעה עגולה של רבע שעה
 INSERT INTO flights (from_city, to_city, flight_date, flight_time, empty_seats)
 SELECT 
-  (ARRAY['Tel Aviv', 'New York', 'London', 'Paris', 'Tokyo', 'Berlin', 'Rome', 'Dubai', 'Bangkok', 'Istanbul'])[floor(random()*10)+1],
-  (ARRAY['New York', 'London', 'Paris', 'Tokyo', 'Berlin', 'Rome', 'Dubai', 'Bangkok', 'Istanbul', 'Tel Aviv'])[floor(random()*10)+1],
+  fc AS from_city,
+  tc AS to_city,
   CURRENT_DATE + (floor(random()*365))::int,
   TO_CHAR(TIME '00:00' + make_interval(mins => 15 * floor(random() * 96)::int), 'HH24:MI'),
   floor(random()*200 + 10)::int
-FROM generate_series(1, 1000);
+FROM (
+  SELECT 
+    (ARRAY['TLV', 'TYO', 'ROM', 'PAR', 'BER', 'MAD', 'DXB', 'NYC', 'IST', 'LON'])[floor(random()*10)+1] AS fc,
+    (ARRAY['TLV', 'TYO', 'ROM', 'PAR', 'BER', 'MAD', 'DXB', 'NYC', 'IST', 'LON'])[floor(random()*10)+1] AS tc
+  FROM generate_series(1, 3000)
+) AS flights_temp
+WHERE fc <> tc
+LIMIT 1000;
+
